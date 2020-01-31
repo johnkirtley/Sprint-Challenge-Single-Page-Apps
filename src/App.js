88979c600c08cb95c382672e6b734b49e66a9e76
johnkirtley <kirtleyj16@gmail.com>
2020-01-31 09:10:57 -0800

    Established route from Welcome Page to Character Cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterCard from "./components/CharacterCard";
+import WelcomePage from "./components/WelcomePage";
+import { Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+      <Route path="/charactercard">
+        <CharacterCard />
+      </Route>
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="search">Search Character Name</label>
+        <input type="text" />
+      </form>
     </section>
   );
 }
